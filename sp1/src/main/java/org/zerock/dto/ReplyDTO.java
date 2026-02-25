300bd057be319cdce62cc7be46fc29c36b8ff5da
JaeH-Y <yjh21175753@gmail.com>
2026-02-25 17:02:28 +0900

    변경: sp1/src/main/java/org/zerock/dto/ReplyDTO.java

---
+++ sp1/src/main/java/org/zerock/dto/ReplyDTO.java
@@ -0,0 +1,22 @@
+package org.zerock.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyDTO {
+	private Long rno;
+	private String replyText;
+	private String replyer;
+	private LocalDateTime replyDate;
+	private LocalDateTime updateDate;
+	private boolean delFlag;
+	private Long bno;
+}
