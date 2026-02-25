04aabf88ec5653347a31c1f2561a95e1e81519b2
JaeH-Y <yjh21175753@gmail.com>
2026-02-25 17:02:28 +0900

    변경: sp1/src/main/java/org/zerock/mapper/BoardMapper.java

---
+++ sp1/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,42 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.zerock.dto.BoardDTO;
+
+public interface BoardMapper {
+
+	int insert(BoardDTO dto);
+	
+	BoardDTO selectOne(Long bno);
+	
+	int removeOne(Long bno);
+	
+	int updateOne(BoardDTO dto);
+	
+	List<BoardDTO> selectList();
+	
+	// MyBatis에서 여러 값을 전달하려면 @Param, Map 타입, 또는 객체(DTO) 타입을 사용
+	// 잘못된 코드
+	// List<BoardDTO> list2(int skip, int count);
+	// 매퍼 XML에서 
+	// LIMIT #{skip}, #{count} -- MyBatis는 skip, count라는 이름을 모름
+	// 1. @Param 사용(권장)
+	// SQL에서 사용할 파라미터 이름을 명시적으로 지정하는 역할, 예: @Param("skip") -> SQL에서 #{skip}
+	List<BoardDTO> list2(@Param("skip") int skip, @Param("count") int count);
+	// 2. Map 사용
+//	List<BoardDTO> list2(Map<String, Integer> param);
+	// 3. DTO 객체 사용(권장)
+//	List<BoardDTO> list2(PageRequestDTO dto);
+	
+	int listCount(@Param("types") String[] types,
+			@Param("keyword") String keyword);
+	
+	List<BoardDTO> listSearch(
+			@Param("skip") int skip,
+			@Param("count") int count,
+			@Param("types") String[] types,
+			@Param("keyword") String keyword);
+	
+}
